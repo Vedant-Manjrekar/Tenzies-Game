addd85959eb502572331594c00b7ad88e1742b2e
Vedant Manjrekar <vedant.28m@gmail.com>
2022-07-04 15:34:49 +0530

    Made Changes for deployment fixes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './css/main.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
 // const root = ReactDOM.createRoot(
 //     document.getElementById('root')
 //   );
